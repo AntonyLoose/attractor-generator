a7fbcd33497af4864a7327d62fcea3f999ae6812
Antony Loose <antony35007@gmail.com>
2025-07-21 18:40:10 +1000

    feat: boiler plate for renderer

---
+++ src/renderer.js
@@ -0,0 +1,23 @@
+export function init_three_animation_cycle(mixer) {
+
+}
+
+export function create_scene(container) {
+
+}
+
+export function clear_scene(scene) {
+
+}
+
+export function add_point(x, y, z, radius = 1, opacity = 0) {
+
+}
+
+export function remove_point(point, scene) {
+
+}
+
+export function render_state_at_t(t, phase_space) {
+
+}
